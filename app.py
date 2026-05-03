1ce046ee1aaf0856ec245d96d68c782f020a826c
dommegagigaco-ctrl <dommegagigaco@gmail.com>
2026-05-03 03:28:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,49 +5,39 @@ from modules.filters import sidebar_location_filter
 st.set_page_config(layout="wide")
 st.title("🏠 Otodom: Łowca Okazji")
 
-# 1. Wczytanie i czyszczenie
+# 1. Wczytanie
 url = "https://docs.google.com/spreadsheets/d/13skyeoJL9MZvM5iCRtHUI7tyfu9BHArse154eizQ_L8/export?format=csv&gid=0"
 df = pd.read_csv(url)
-if 'id' in df.columns:
-    df = df.drop_duplicates(subset=['id'], keep='last')
 
-# 2. Przetwarzanie dat
-for col in ['dateCreated', 'listingDetails/dateCreated']:
-    df[col] = pd.to_datetime(df[col], errors='coerce')
+# 2. Inteligentne usuwanie duplikatów (id to unikalny klucz)
+df = df.drop_duplicates(subset=['id'], keep='last')
 
+# 3. Wybór NAJLEPSZYCH dat (używamy globalnych, są najbardziej stabilne)
+df['dateCreated'] = pd.to_datetime(df['dateCreated'], errors='coerce')
 df['dni_na_rynku'] = (pd.Timestamp.now() - df['dateCreated']).dt.days
 
-# 3. Filtrowanie
+# 4. Filtry
 df_f = sidebar_location_filter(df)
 
-# 4. Tabela z klikalnym tytułem i nowymi danymi
+# 5. Konfiguracja tabeli (tylko to co niezbędne)
 cols = {
-    'title': 'Tytuł\noferty',
-    'price': 'Cena\n(PLN)',
-    'listingDetails/pricePerSquareMeter/value': 'Cena\nza m²',
-    'listingDetails/areaInSquareMeters': 'Metraż\n(m²)',
-    'dni_na_rynku': 'Dni\nrynku',
+    'title': 'Tytuł oferty',
+    'price': 'Cena (PLN)',
+    'listingDetails/pricePerSquareMeter/value': 'Cena/m²',
+    'dni_na_rynku': 'Wiek (dni)',
     'floor': 'Piętro',
     'listingDetails/location/address/street/name': 'Ulica',
-    'listingDetails/location/reverseGeocoding/locations/2/name': 'Lokalizacja\nLvl 2',
-    'listingDetails/location/reverseGeocoding/locations/3/name': 'Lokalizacja\nLvl 3',
+    'listingDetails/location/reverseGeocoding/locations/3/name': 'Lokalizacja Lvl 3',
     'listingDetails/development/title': 'Deweloper',
-    'listingDetails/development/investmentState': 'Stan\ninw.'
+    'url': 'Link'
 }
 
-# Wyświetlanie - używamy LinkColumn dla 'title', mapując go na 'url'
+# Wyświetl
 st.dataframe(
     df_f[list(cols.keys())],
     column_config={
-        "title": st.column_config.LinkColumn("Tytuł oferty", display_text="title"),
-        # Tutaj sztuczka: łączymy tytuł z linkiem
-        "title": st.column_config.LinkColumn(
-            "Tytuł oferty",
-            help="Kliknij, aby otworzyć ofertę",
-            validate="^https://",
-            display_text="title" # To wymaga małej poprawki - Streamlit automatycznie zrobi link z kolumny url
-        ),
-        # ... konfiguracja pozostałych kolumn
+        "title": st.column_config.LinkColumn("Tytuł", display_text="title"),
+        "url": st.column_config.LinkColumn("Link", display_text="Otwórz")
     },
     use_container_width=True
 )
