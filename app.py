f4411d8ddd2728295ea09020f773c0c6533990f2
dommegagigaco-ctrl <dommegagigaco@gmail.com>
2026-05-03 03:31:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,39 +5,41 @@ from modules.filters import sidebar_location_filter
 st.set_page_config(layout="wide")
 st.title("🏠 Otodom: Łowca Okazji")
 
-# 1. Wczytanie
+# Wczytanie i duplikaty
 url = "https://docs.google.com/spreadsheets/d/13skyeoJL9MZvM5iCRtHUI7tyfu9BHArse154eizQ_L8/export?format=csv&gid=0"
 df = pd.read_csv(url)
+if 'id' in df.columns:
+    df = df.drop_duplicates(subset=['id'], keep='last')
 
-# 2. Inteligentne usuwanie duplikatów (id to unikalny klucz)
-df = df.drop_duplicates(subset=['id'], keep='last')
-
-# 3. Wybór NAJLEPSZYCH dat (używamy globalnych, są najbardziej stabilne)
+# Przetwarzanie
+df['url'] = df['url'].str.replace('/ad/', '/oferta/')
 df['dateCreated'] = pd.to_datetime(df['dateCreated'], errors='coerce')
 df['dni_na_rynku'] = (pd.Timestamp.now() - df['dateCreated']).dt.days
 
-# 4. Filtry
 df_f = sidebar_location_filter(df)
 
-# 5. Konfiguracja tabeli (tylko to co niezbędne)
+# Definicja kolumn
 cols = {
+    'url': 'Link',
     'title': 'Tytuł oferty',
-    'price': 'Cena (PLN)',
+    'price': 'Cena',
     'listingDetails/pricePerSquareMeter/value': 'Cena/m²',
-    'dni_na_rynku': 'Wiek (dni)',
+    'dni_na_rynku': 'Dni',
     'floor': 'Piętro',
     'listingDetails/location/address/street/name': 'Ulica',
-    'listingDetails/location/reverseGeocoding/locations/3/name': 'Lokalizacja Lvl 3',
+    'listingDetails/location/reverseGeocoding/locations/3/name': 'Lokalizacja',
     'listingDetails/development/title': 'Deweloper',
-    'url': 'Link'
+    'listingDetails/development/investmentState': 'Stan inw.'
 }
 
-# Wyświetl
+# Wyświetlanie
 st.dataframe(
     df_f[list(cols.keys())],
     column_config={
-        "title": st.column_config.LinkColumn("Tytuł", display_text="title"),
-        "url": st.column_config.LinkColumn("Link", display_text="Otwórz")
+        "url": st.column_config.LinkColumn("Link", display_text="Otwórz"),
+        "price": st.column_config.NumberColumn("Cena", format="%d"),
+        "listingDetails/pricePerSquareMeter/value": st.column_config.NumberColumn("Cena/m²", format="%d"),
+        "dni_na_rynku": st.column_config.NumberColumn("Dni", format="%d")
     },
     use_container_width=True
 )
